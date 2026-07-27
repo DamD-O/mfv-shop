26d0eb9f2ed8801942d8ab2fa2a3c038f4284b18
yedam <yedam5063@gmail.com>
2026-07-27 20:13:33 +0900

    초기 커밋

---
+++ src/main/java/com/example/shop/FmfApplication.java
@@ -0,0 +1,17 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing //Entity가 저장/수정될 때마다 자동으로 현재 시각을 채워줌
+@SpringBootApplication
+public class FmfApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FmfApplication.class, args);
+    }
+
+}
