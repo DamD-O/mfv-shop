f002a63f9e1b144bbe477b48d74b353072bb92a4
yedam <yedam5063@gmail.com>
2026-07-30 14:29:14 +0900

    권한 수정 권한 테이블 -> enum 으로 수정

---
+++ src/main/java/com/example/shop/domain/admin/entity/AuthType.java
@@ -0,0 +1,28 @@
+package com.example.shop.domain.admin.entity;
+
+import lombok.Getter;
+
+/**
+ * @author yedam
+ * @DATE 2026-07-29
+ * @description 권한 타입 Enum
+ */
+
+@Getter
+public enum AuthType
+{
+    ADMIN("관리자"),
+    USER("사용자");
+
+    private String label;
+
+    AuthType(String label)
+    {
+        this.label = label;
+    }
+
+    public String getLabel()
+    {
+        return label;
+    }
+}
