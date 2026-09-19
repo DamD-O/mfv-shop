e811dd8c784842836fe5494ad54c716eddc9ce2f
yedam <yedam5063@gmail.com>
2026-09-19 11:12:39 +0900

    네비게이션바 드롭다운 공통함수 추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -27,14 +27,21 @@ async function checkLoginStatus() {
 
         if (response.ok) {
             if (data.role === 'ROLE_ADMIN') {
-                myBtn.onclick = () => location.href = 'admin.html';
                 document.getElementById('my-status').innerText = '관리자페이지'
+                document.getElementById('my-dropdown').innerHTML = `
+                    <li><a class="dropdown-item" href="admin.html">관리자페이지</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="logout()">로그아웃</a></li>
+                `;
 
                 footerLoginLink.href = 'admin.html';
                 footerLoginLink.textContent = '관리자페이지';
             } else {
-                myBtn.onclick = () => location.href = 'mypage.html';
-                document.getElementById('my-status').innerText = '마이페이지'
+                document.getElementById('my-status').innerText = data.name + '님'
+                document.getElementById('my-dropdown').innerHTML = `
+                    <li><a class="dropdown-item" href="mypage.html">마이페이지</a></li>
+                    <li><a class="dropdown-item" href="order.html">주문내역</a></li>
+                    <li><a class="dropdown-item" href="#" onclick="logout()">로그아웃</a></li>
+                `;
 
                 footerLoginLink.textContent = '마이페이지';
                 footerLoginLink.href = 'mypage.html';
@@ -75,9 +82,20 @@ async function fetchWithAuth(url, options = []) {
 
     if (response.status === 401) {
         alert('로그인이 필요합니다.');
-        location.href = 'login.html';
+        location.href = `login.html?redirect=${encodeURIComponent(location.href)}`;
         return null;
     }
 
     return response;
+}
+
+async function logout() {
+    const response = await fetch('/api/logout', {
+        method: 'POST',
+        credentials: 'include'
+    });
+
+    if (response.ok) {
+        location.href = 'index.html';
+    }
 }
